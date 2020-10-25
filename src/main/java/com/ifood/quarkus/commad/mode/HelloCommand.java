732a39a947923ebc2a1fae96df6255ad8fb84479
breno500as <breno500as@gmail.com>
2020-10-25 18:35:03 -0300

    quarkus

--- src/main/java/com/ifood/quarkus/commad/mode/HelloCommand.java
+++ src/main/java/com/ifood/quarkus/commad/mode/HelloCommand.java
@@ -1,15 +1,30 @@
 package com.ifood.quarkus.commad.mode;
 
+import java.util.stream.Stream;
+
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+
+import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 
 @QuarkusMain
 public class HelloCommand implements QuarkusApplication {
+	
+	
+	@Inject
+	@Channel("parametros")
+	private Emitter<String> emitter;
 
+	// java  -jar arquivo.jar -Dquarkus.args=a -Dquarkus.args=b -Dquarkus.args=c
     @Override
     public int run(String... args) throws Exception {
-        final String name = args.length > 0 ? String.join(" ", args) : "commando";
-        System.out.println("hello " + name);
+    	System.out.println(args.length);
+        Stream.of(args).forEach(arg -> this.emitter.send(arg));
+        Quarkus.waitForExit();
         return 0;
     }
 }
