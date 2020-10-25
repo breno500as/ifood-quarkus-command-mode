028e0a43f4499a4bb1cfece66ccb995993ecaf93
breno500as <breno500as@gmail.com>
2020-10-25 17:42:21 -0300

    quarkus

---
+++ src/main/java/com/ifood/quarkus/commad/HelloCommand.java
@@ -0,0 +1,15 @@
+package com.ifood.quarkus.commad;
+
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class HelloCommand implements QuarkusApplication {
+
+    @Override
+    public int run(String... args) throws Exception {
+        final String name = args.length > 0 ? String.join(" ", args) : "commando";
+        System.out.println("hello " + name);
+        return 0;
+    }
+}
